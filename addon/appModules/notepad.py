ad792d285e31356c9576962dec2a8a80b500a003
Meron Woldesenbet <119830075+Meronats@users.noreply.github.com>
2024-02-14 10:31:07 -0600

    notepad

---
+++ addon/appModules/notepad.py
@@ -0,0 +1,13 @@
+#accessible addon
+
+# Add-on development first example
+
+import appModuleHandler
+
+class AppModule(appModuleHandler.AppModule):
+
+	def event_gainFocus(self, obj, nextHandler):
+		import tones
+		tones.beep(550, 50)
+		nextHandler()
+		
