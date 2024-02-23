2dc608935a21b541b5ded5faf4818ec6a139f9ae
Meron Woldesenbet <119830075+Meronats@users.noreply.github.com>
2024-02-23 10:54:24 -0600

    caret

--- addon/appModules/pycharm64.py
+++ addon/appModules/pycharm64.py
@@ -8,10 +8,23 @@ class AppModule(appModuleHandler.AppModule):
     def event_gainFocus(self, obj, nextHandler):
         tones.beep(550, 50)
         nextHandler()
-
-    def event_stateChange(self, obj, nextHandler):
-        
-        if obj.role == controlTypes.ROLE_EDITABLETEXT and obj.states:
-           
-            tones.beep(660, 50)
+   
+    def event_caret(self, obj, nextHandler):
+        current_line_text = self.get_current_line_text(obj)
+        if current_line_text is not None:  
+         indentation_level = self.get_indentation_level(current_line_text)
+         self.announce_indentation(indentation_level)
         nextHandler()
+
+    def get_current_line_text(self, obj):
+        pass
+
+    def get_indentation_level(self, line_text):
+        indentation_level = len(line_text) - len(line_text.lstrip(' '))
+        return indentation_level // 4
+
+    def announce_indentation(self, level):
+        if level > 0:
+            tones.beep(440 + (level * 20), 100)
+        else:
+            pass
