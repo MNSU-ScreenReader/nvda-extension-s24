21e588263ab551861ba54a898b34eaae432f4f1e
Meron Woldesenbet <119830075+Meronats@users.noreply.github.com>
2024-02-22 22:37:44 -0600

    pycharm

--- addon/appModules/pycharm68.py
+++
@@ -1,8 +0,0 @@
-import appModuleHandler
-
-class AppModule(appModuleHandler.AppModule):
-
-    def event_gainFocus(self, obj, nextHandler):
-        import tones
-        tones.beep(550, 50)
-        nextHandler()
