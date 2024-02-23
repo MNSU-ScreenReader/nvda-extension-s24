c6474fa9921a689a053388602b510450b8ffb6d3
Meron Woldesenbet <119830075+Meronats@users.noreply.github.com>
2024-02-22 22:27:51 -0600

    notepad

--- addon/appModules/notepad.py
+++ addon/appModules/notepad.py
@@ -1,8 +1,17 @@
 import appModuleHandler
+import tones
+import api
+import controlTypes
 
 class AppModule(appModuleHandler.AppModule):
 
     def event_gainFocus(self, obj, nextHandler):
-        import tones
         tones.beep(550, 50)
+        nextHandler()
+
+    def event_stateChange(self, obj, nextHandler):
+        
+        if obj.role == controlTypes.ROLE_EDITABLETEXT and obj.states:
+           
+            tones.beep(660, 50)
         nextHandler()
---
+++ addon/appModules/pycharm68.py
@@ -0,0 +1,8 @@
+import appModuleHandler
+
+class AppModule(appModuleHandler.AppModule):
+
+    def event_gainFocus(self, obj, nextHandler):
+        import tones
+        tones.beep(550, 50)
+        nextHandler()
