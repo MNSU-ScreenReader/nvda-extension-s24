f56ef7e281f7615d62309383a260bd01040c65d4
Meron Woldesenbet <119830075+Meronats@users.noreply.github.com>
2024-03-26 12:46:42 -0500

    Tab beep

--- addon/appModules/pycharm64.py
+++ addon/appModules/pycharm64.py
@@ -10,6 +10,12 @@ class AppModule(appModuleHandler.AppModule):
     def event_gainFocus(self, obj, nextHandler):
         tones.beep(550, 50)
         nextHandler()
+    
+    def script_boopOnTab(self, gesture):
+        focusObj = api.getFocusObject()
+        if isinstance(focusObj, NVDAObjects.IAccessible.IAccessible) and focusObj.role == controlTypes.Role.EDITABLETEXT:
+            tones.beep(440, 100)  
+        gesture.send()
    
     def script_reportIndentation(self, gesture):
         obj = api.getNavigatorObject()
@@ -28,5 +34,6 @@ class AppModule(appModuleHandler.AppModule):
     def __init__(self, *args, **kwargs):
         super(AppModule, self).__init__(*args, **kwargs)
         self.bindGesture("kb:space", "reportIndentation")
-    
+   
+
   
