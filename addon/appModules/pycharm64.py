2a28e3e6236b84723c13f826d31f87d88e2bf68f
sbaeye19 <samuel.baeyen@mnsu.edu>
2024-03-31 21:21:17 -0500

    EditorText added
    gets the current line of the caret
    TODO: fix so it gets the entire python file

--- addon/appModules/pycharm64.py
+++ addon/appModules/pycharm64.py
@@ -1,22 +1,42 @@
+import sys,os
 import appModuleHandler
-import speech
+import eventHandler
+import scriptHandler
+import tones
 import api
-import identation_parser
-
+import ui
+import controlTypes
+import textInfos
+import speech
+import weakref
+from queueHandler import registerGeneratorObject
+from NVDAObjects.IAccessible import IAccessible
+from editableText import EditableTextWithoutAutoSelectDetection
+from NVDAObjects.behaviors import EditableTextWithAutoSelectDetection as EditWindowBaseCls
+from NVDAObjects.behaviors import EditableTextWithSuggestions
+parser = None
 class AppModule(appModuleHandler.AppModule):
-    def event_gainFocus(self, obj, nextHandler):
-        import tones
-        tones.beep(550, 50)
-        nextHandler()
+    def __init__(self, pid, appName=None):
+        super(AppModule, self).__init__(pid, appName)
+        
+    def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+        if obj.role == controlTypes.ROLE_EDITABLETEXT:
+            print("In the editor")
+            clsList.insert(0, PyCharmEditWindow)
 
-    def script_doSpeak(self, gesture):
-        try:
-            focusedObject = api.getFocusObject()
-            text = focusedObject.value
-            speech.speakText("")
-        except AttributeError:
-            speech.speakText("No text Found")
+    
 
-    __gestures={
-        "kb:NVDA+A": "doSpeak"
-    }
+class PyCharmEditWindow(EditWindowBaseCls, EditableTextWithSuggestions):
+
+    def event_gainFocus(self):
+        super(PyCharmEditWindow, self).event_gainFocus()
+        self.appModule.edit = None
+        self.appModule._edit = weakref.ref(self)
+
+    def event_caret(self):
+        super(PyCharmEditWindow, self).event_caret()
+        tones.beep(330,50)
+        info = self.makeTextInfo(textInfos.POSITION_ALL)
+        info.expand(textInfos.UNIT_LINE)
+
+    
