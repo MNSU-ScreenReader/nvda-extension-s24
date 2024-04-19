42933c84e3add2504a42df45c2445e2af4957665
sbaeye19 <samuel.baeyen@mnsu.edu>
2024-04-19 01:35:27 -0500

    Added function name output utilizing ast

--- addon/appModules/pycharm64.py
+++ addon/appModules/pycharm64.py
@@ -15,8 +15,10 @@ from NVDAObjects.IAccessible import IAccessible
 from editableText import EditableTextWithoutAutoSelectDetection
 from NVDAObjects.behaviors import EditableTextWithAutoSelectDetection as EditWindowBaseCls
 from NVDAObjects.behaviors import EditableTextWithSuggestions
-class IndentationParser:
 
+editor_file = ""
+current_idx = -1
+class IndentationParser:
     def __init__(self, prgm_text: str):
         self.prgm_text: str = prgm_text
 
@@ -35,8 +37,9 @@ class IndentationParser:
                 current_indentation = len(line) - len(line.lstrip())
                 if current_indentation > 0:
                     count += 1
-                    indentation = current_indentation
+
                     if(current_line == line):
+                        indentation = current_indentation
                         current_idx = idx
 
                     if current_indentation % majority_indentation != 0:
@@ -44,7 +47,7 @@ class IndentationParser:
                             continue
                         print("error line:", idx, " change to", majority_indentation)
                         return majority_indentation, idx
-        return indentation, current_idx
+        return indentation/majority_indentation, current_idx
     
     def get_indentation_statistics(self):
         indentation_count = {}
@@ -75,51 +78,47 @@ class AppModule(appModuleHandler.AppModule):
 
     def chooseNVDAObjectOverlayClasses(self, obj, clsList):
         if obj.role == controlTypes.ROLE_EDITABLETEXT:
-            #print("In the editor")
+            print("In the editor")
             clsList.insert(0, PyCharmEditWindow)
 
 class PyCharmEditWindow(EditWindowBaseCls, EditableTextWithSuggestions):
-    editor_file = ""
     def event_gainFocus(self):
         super(PyCharmEditWindow, self).event_gainFocus()
         self.appModule.edit = None
         self.appModule._edit = weakref.ref(self)
+
     def event_caret(self):
         super(PyCharmEditWindow, self).event_caret()
         tones.beep(330,50)
-
+        global editor_file
         info = self.makeTextInfo(textInfos.POSITION_ALL)
         info2 = self.makeTextInfo(textInfos.POSITION_CARET)
 
         #Stores the file that's in the editor
-        self.editor_file = info.text
+        editor_file = info.text
         info2.expand(textInfos.UNIT_LINE)
         
         #Gets the current line and indentation level of the caret
         current_line = info2.text.strip('\n')
         current = len(current_line) - len(current_line.lstrip())
-
+        
         #Parses for the indentation level of the current line, dividing by majority to get the level
-        parser = IndentationParser(self.editor_file)
+        parser = IndentationParser(editor_file)
+        global current_idx
         indentation, current_idx = parser.get_indentation(current_line)
-        current = int(current/indentation)
-        self.current_idx = current_idx
         #Reads aloud indentation level of current line
-        speech.speakText(f'Indentation is {current}') 
+        speech.speakText(f'Indentation is {int(indentation)}') 
         indentation_percentages, majority_indentation = parser.get_indentation_statistics()
-        #print(indentation_percentages)
-        self.findFunction()
-        #print(f'Found: {found} Name:{Fname}')
 
     def script_detectFunction(self,gesture):
-        self.editor_file
+        #print("Editor File:", editor_file)
+        tree = ast.parse(editor_file)
+        for node in ast.walk(tree):
+            if isinstance(node, ast.FunctionDef):
+                # Check if the current_idx is within the range of this function's lines
+                if node.lineno <= current_idx:
+                    function_name = node.name
+        speech.speakText(f'In Function {function_name}')
     __gestures = {
         "kb:NVDA+I": "detectFunction",
     }
-    
-    def findFunction(self):
-        tree = ast.parse("".join(self.editor_file))
-
-        for node in ast.walk(tree):
-            if isinstance(node, ast.FunctionDef):
-                print("hello")
